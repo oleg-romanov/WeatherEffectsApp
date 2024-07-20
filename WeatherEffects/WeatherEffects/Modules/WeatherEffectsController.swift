2140792981e1285c6378b79a003e780f0317e68b
Oleg Romanov <olegromanov.3003@gmail.com>
2024-07-20 14:51:31 +0300

    Add localization

--- WeatherEffects/WeatherEffects/Modules/WeatherEffectsController.swift
+++ WeatherEffects/WeatherEffects/Modules/WeatherEffectsController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class WeatherEffectsController: UIViewController {
+    
+    // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+enum WeatherEffects: String, CaseIterable {
+    case clear
+    case rain
+    case cloudy
+    case thunderStorm
+    case snow
+    case snowWithRain
+    case blizzard
+    case fog
+    
+    var localized: String {
+        get {
+            switch self {
+                case .clear:
+                    return LocalizableStrings.clear
+                case .rain:
+                    return LocalizableStrings.rain
+                case .cloudy:
+                    return LocalizableStrings.cloudy
+                case .thunderStorm:
+                    return LocalizableStrings.thunderStorm
+                case .snow:
+                    return LocalizableStrings.snow
+                case .snowWithRain:
+                    return LocalizableStrings.snowWithRain
+                case .blizzard:
+                    return LocalizableStrings.blizzard
+                case .fog:
+                    return LocalizableStrings.fog
+            }
+        }
+    }
+}
---
+++ WeatherEffects/WeatherEffects/Resources/LocalizableStrings.swift
@@ -0,0 +1,19 @@
+//
+//  LocalizableStrings.swift
+//  WeatherEffects
+//
+//  Created by Олег Романов on 20.07.2024.
+//
+
+import Foundation
+
+struct LocalizableStrings {
+    static let clear = NSLocalizedString("clear", comment: "")
+    static let rain = NSLocalizedString("rain", comment: "")
+    static let cloudy = NSLocalizedString("cloudy", comment: "")
+    static let thunderStorm = NSLocalizedString("thunderStorm", comment: "")
+    static let snow = NSLocalizedString("snow", comment: "")
+    static let snowWithRain = NSLocalizedString("snowWithRain", comment: "")
+    static let blizzard = NSLocalizedString("blizzard", comment: "")
+    static let fog = NSLocalizedString("fog", comment: "")
+}
